e8b23f0fbc0f21bcd82bad3a374739d093e7b3f3
elpeace8787 <36679578+elpeace8787@users.noreply.github.com>
2025-07-25 13:48:47 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
+// Select all sidebar links
+const sidebarLinks = document.querySelectorAll('.sidebar a');
 
+// Add click event to each link
+sidebarLinks.forEach(link => {
+  link.addEventListener('click', function () {
+    // Remove 'active' class from all links
+    sidebarLinks.forEach(link => link.classList.remove('active'));
+
+    // Add 'active' class to the clicked link
+    this.classList.add('active');
+  });
+});
+
+// Highlight the current page based on URL
+const currentPage = window.location.pathname.split('/').pop();
+sidebarLinks.forEach(link => {
+  if (link.getAttribute('href') === currentPage) {
+    link.classList.add('active');
+  }
+});
